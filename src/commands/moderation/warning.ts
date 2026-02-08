c06fd95c912f15377bc04e73d14109ccb3868b8e
FilipeSilveira-Tech <sanzinho.yoshimoto@outlook.com>
2026-02-08 01:44:31 -0300

    refactor: warning command

--- src/commands/moderation/warning.ts
+++ src/commands/moderation/warning.ts
@@ -4,11 +4,13 @@ import {
     ChatInputCommandInteraction,
     MessageFlags,
     EmbedBuilder,
-    TextChannel
+    TextChannel,
+    GuildMember
 } from "discord.js";
 import { Command } from "../../structs/types/client.js";
 import { initFirestore } from "../../lib/firestore.js";
-import { getOrCreateWarningUser } from "../../lib/database/defaultUser.schema.js";
+import { count } from "node:console";
+import { FieldValue } from "firebase-admin/firestore";
 const db = initFirestore();
 
 const warningCommand: Command = {
@@ -29,110 +31,100 @@ const warningCommand: Command = {
                 .setRequired(false),
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-    async execute(interaction: ChatInputCommandInteraction) {
-        const target = interaction.options.getUser('target');
-        const reason = interaction.options.getString('reason') ?? "Nenhuma motivo fornecido";
-        const { guild } = interaction;
-        const userId = target!.id;
-        const guildId = guild!.id;
 
-        const guildDoc = await db.collection('guilds').doc(String(guildId)).get();
-        const config = guildDoc.data();
-        const moderationSystem = config?.modules?.moderation?.enabled;
-        const warningSystem = config?.modules?.moderation?.warningSystem;
+    async execute(interaction: ChatInputCommandInteraction) {
+       const { guild, user, options } = interaction;
+
+       if (!guild) return;
+
+       // Coleta de daddos
+       const targetUser = options.getUser('target', true);
+       const reason = options.getString('reason') ?? "Sem motivo definido";
+       const targetMember = await guild.members.fetch(targetUser.id).catch(() => null);
+
+       if (!targetMember) {
+        interaction.reply({ content: '❌ Usuário não encontrado no servidor!', flags: MessageFlags.Ephemeral });
+        return;
+       }
+
+       if (targetUser.id === user.id) {
+        interaction.reply({ content: '❌ Você não pode avisar a si mesmo', flags: MessageFlags.Ephemeral });
+        return;
+       }
+
+       if (targetUser.bot) {
+        interaction.reply({ content: '❌ Você não pode avisar um bot!', flags: MessageFlags.Ephemeral });
+        return;
+       }
+
+       // Verifica se o alvo é cargo maior ou igual a moderador
+       const executorMembers = interaction.member as GuildMember;
+       if (targetMember.roles.highest.position >= executorMembers.roles.highest.position) {
+        interaction.reply({ content: '❌ Você não pode dar aviso à alguém com cargo maior que o seu!', flags: MessageFlags.Ephemeral });
+        return;
+       }
+
+       // Consulta de configuração:
+       const guildRef = db.collection('guilds').doc(guild.id);
+       const guildSnap = await guildRef.get();
+       const config = guildSnap.data();
+
+       // Verifica os módulos
+       if (!config?.modules?.moderation?.enabled) {
+        interaction.reply({ content: '⚠️ O módulo de moderação está desativado.', flags: MessageFlags.Ephemeral });
+        return;
+       };
+
+        if (!config?.modules?.moderation?.warningSystem) {
+            interaction.reply({ content: '⚠️ O módulo de warning está desativado.', flags: MessageFlags.Ephemeral });
+            return;
+        };
+
+        // Operação no banco de dados
+        // Estrutura: users/{userId}/warnings/{guildId} -> Permite saber quantos warners por servidor
+        const warningRef = db.collection('users').doc(targetUser.id).collection('warnings').doc(guild.id);
+
+        // Usando set com merge para garantir que o doc exista e incrementa!
+        await warningRef.set({
+            warningCount: FieldValue.increment(1),
+            lastReason: reason,
+            updatedAt: new Date()
+        }, { merge: true });
+
+        //
+        const updatedSnap = await warningRef.get();
+        const newCount = updatedSnap.data()?.count || 1;
+
+        //EMBED: Warning
+        const warningEmbed = new EmbedBuilder()
+            .setTitle('⚠️ Usuário Advertido')
+            .setColor('#FFAA00') // Laranja é comum para warns, Vermelho para ban
+            .addFields(
+                { name: '👤 Usuário', value: `${targetUser.tag} (${targetUser.id})`, inline: true },
+                { name: '🛡️ Moderador', value: `${user.tag}`, inline: true },
+                { name: '🔢 Total de Avisos', value: `${newCount}`, inline: true },
+                { name: '📝 Motivo', value: reason }
+            )
+            .setTimestamp()
+            .setThumbnail(targetUser.displayAvatarURL());
+
+        // Verifica Canal de log
         const logChannelId = config?.modules?.moderation?.logChannelId;
 
-        if (moderationSystem === false) { // Verifica se modulo de moderação estão ativados
-            await interaction.reply({
-                content: 'O modulo de moderação do servidor está desligado, favor solicitar alguém com permissão para ligar o modulo para usar o comando!',
-                flags: MessageFlags.Ephemeral
-            });
-            return;
-        } else if ( warningSystem === false) { // Verifica se modulo de aviso está ligado
-            await interaction.reply({
-                content: 'O modulo de aviso está desligado! Favor solicitar para alguém com permissão para ligar o modulo para usar o comando!',
-                flags: MessageFlags.Ephemeral
-            });
-            return;
-        }
-
-        // Sistemas todos ligados
-        const warningData = await getOrCreateWarningUser(String(userId), String(guildId));
-        const currentWarning = warningData.warningCount ?? 0;
-        const newWarningCount = currentWarning + 1;
-
-        if (!guild || !target) return;
-        const member = await guild.members.fetch(target.id);
-
-        // VERIFICAÇÃO: Usuário é cargo maior!
-        if (!member.bannable && !member.kickable) {
-            interaction.reply({
-                content: "❌ Eu não posso adicionar um aviso a esse membro!\n Ele pode ser cargo superior!",
-                flags: MessageFlags.Ephemeral
-            });
-            return;
-        };
-
-        const userRef = await db.collection('users').doc(String(target.id)).collection('warning').doc(String(guild.id));
-        const snap = await userRef.get();
-
-        //EMEBED: Warning User!
-        const warningEmbed = new EmbedBuilder()
-            .setTitle('⚠️ Usuário Notificado!')
-            .setColor('#FF0000')
-            .addFields(
-                {
-                    name: 'Usuario',
-                    value: `${target.tag} ID: ${target.id}`,
-                    inline: true
-                },
-                {
-                    name: 'Moderador',
-                    value: `${interaction.user.tag} ID: ${interaction.user.id}`,
-                    inline: true
-                },
-                {
-                    name: 'Qtd Aviso',
-                    value: `${newWarningCount}`,
-                    inline: true
-                },
-                {
-                    name: 'Razão',
-                    value: reason
-                },)
-            .setTimestamp();
-
-        // VERIFICAÇÃO: Usuário nunca sofreu aviso!
-        if (!snap.exists) {
-            await userRef.set({
-                warningCount: 1,
-                updateAt: new Date()
-            });
-            // Verifica se possui o canal e envia o log e retorna
-            if (logChannelId) {
-                const logChannel = guild.channels.cache.get(logChannelId) as TextChannel;
-                await logChannel.send({ embeds: [warningEmbed] })
-                return;
-            };
-        };
-
-        // Faz o update do aviso do usuário
-        await userRef.update({
-            warningCount: newWarningCount
-        });
-
-        // Verifica se possui o canal de log
+        // Tenta enviar ao canal de log
         if (logChannelId) {
             const logChannel = guild.channels.cache.get(logChannelId) as TextChannel;
-            await logChannel.send({ embeds: [warningEmbed] })
-            await interaction.reply()
-        } else {
-            // Sem canal de log responde no proprio chat!
-            await interaction.reply({
-                embeds: [warningEmbed],
-                flags: MessageFlags.Ephemeral
-            });
-        };
+            if (logChannel) {
+                await logChannel.send({ embeds: [warningEmbed] }).catch(() => null);
+            };
+        }
+
+        // Resposta Final:
+        await interaction.reply({
+            content: `✅ Aviso aplicado com sucesso para **${targetUser.tag}**.`,
+            embeds: [warningEmbed],
+            flags: MessageFlags.Ephemeral
+        });
     }
 }
 
